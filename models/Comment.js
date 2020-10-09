7d52860afbd05158cd994ff7ebfd26cfe4ef97ce
EK <ekim0480@gmail.com>
2020-10-08 19:45:21 -0500

    functioning first page render

--- models/Comment.js
+++ models/Comment.js
@@ -18,7 +18,7 @@ module.exports = function (sequelize, DataTypes) {
     };
 
     Comment.associate = function(models) {
-      Calendar.hasMany(models.Calendar, {
+      Comment.hasMany(models.Calendar, {
       });
     };
   
