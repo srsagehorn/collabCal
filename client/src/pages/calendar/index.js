5c16752762fde4f738484ae31a467e425231b047
Shannon Sagehorn <srsagehorn@gmail.com>
2020-10-10 13:20:57 -0500

    styling link fixed

--- client/src/pages/calendar/index.js
+++ client/src/pages/calendar/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Cal from "../../components/calendar";
 import NewEvent from "../../components/newEvent";
+import Nav from "../../components/nav";
 // import Footer from "../../components/Footer"
 
 export default function () {
   return (
     <div>
+      <Nav />
       <Cal />
       <NewEvent />
     </div>
--- client/src/pages/createGroup/index.js
+++ client/src/pages/createGroup/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CreateGroup from "../../components/createGroup";
+import Nav from "../../components/nav";
 // import Footer from "../../components/Footer"
 
 export default function () {
   return (
     <div>
+      <Nav />
       <CreateGroup />
     </div>
   );
