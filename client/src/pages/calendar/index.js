f74c673697afd850a27154deba18bd70d2fdbeb4
Kieran Lynch <lynch.kieran1@gmail.com>
2020-10-12 22:28:10 -0500

    linked create a group client api to db

--- client/src/pages/calendar/index.js
+++ client/src/pages/calendar/index.js
@@ -5,7 +5,7 @@ import Nav from "../../components/nav";
 // import Footer from "../../components/Footer"
 
 export default function () {
-  // API get request from events table. store in array. pass array into the CAL tag below. 
+  // API 'get' request from events table. store in array. pass array into the CAL tag below (as a prop?). 
 
   return (
     <div>
