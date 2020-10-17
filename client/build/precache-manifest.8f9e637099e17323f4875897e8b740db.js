59463d39d64deb1bf9cbc0ef51a4afb966d015a7
Shannon Sagehorn <srsagehorn@gmail.com>
2020-10-17 13:43:29 -0500

    build folders

---
+++ client/build/precache-manifest.8f9e637099e17323f4875897e8b740db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a85d75d9da649e41d71f",
+    "url": "/static/css/main.e586cab8.chunk.css"
+  },
+  {
+    "revision": "a85d75d9da649e41d71f",
+    "url": "/static/js/main.6764084b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9b5216630969122f7def",
+    "url": "/static/css/2.78f8a055.chunk.css"
+  },
+  {
+    "revision": "9b5216630969122f7def",
+    "url": "/static/js/2.d3770d84.chunk.js"
+  },
+  {
+    "revision": "3b0cb7a6757f4b168eeae21f4d5cbe30",
+    "url": "/index.html"
+  }
+];
